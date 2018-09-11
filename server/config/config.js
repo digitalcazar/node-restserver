514997816016d3ba32c437294b61b001a0732f43
digitalcazar <digitalcazar@gmail.com>
2018-09-11 09:46:58 -0500

    Primer commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,8 @@
-//Aqui nosotros vamos a declarar constantes, variables arreglos, vamos, todo lo que vamos a necesitar en forma global
-//Existe una funcion que se llama process y en todo momento esta ejecutando en nuestro entorno, conoce el puerto por
-//el cual esta escuchando en todo momento, pero puede ser que no lo encuentre en un momento dado y lo podemos
+// Aqui nosotros vamos a declarar constantes, variables arreglos, 
+// vamos, todo lo que vamos a necesitar en forma global
+// Existe una funcion que se llama process y en todo momento esta ejecutando
+// en nuestro entorno, conoce el puerto por el cual esta escuchando en todo
+// momento, pero puede ser que no lo encuentre en un momento dado y lo podemos
 //asignar el puerto 3000
 
 //=========
--- server/server.js
+++ server/server.js
@@ -1,60 +1,50 @@
-//Al ser esta nuetra primera linea, va a ir al archivo de config y va a ejecutar el archivo config.js
+// Al ser este nuestra primera linea, va a ir al archivo de config y va a ejecutar el
+// archivo congif.js
 require('./config/config');
 
-//Primero vamos a cambiar el var por un const y agregar un ";" al final de cada linea
-//Es un estandar o norma que todos los requiere vayan hasta arriba en los documentos js
+// Primero vamos a cambiar el var por un const, y agregar un ";" al final de cada linea
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
-//Agregamos el body-parser
+// Agregamos el body-parser
 const bodyParser = require('body-parser');
-//Siempre que se hace una peticion pasa por aqui y se ejecuta siempre estas lineas del body-parser a esto se le llama Middleware
+// Siempre que se hace una peticion pasa por aqui y se ejecuta siempre estas lineas del body-parser a
+// esto se le llama Middleware
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-    // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
-//Cambiar la palabra send por json
-//Agregamos /usuario y cambiamos el texto de salida
+// Cambiar la palabra send por json
+// Agregamos /usuario y cambiamos el texto de salida
 app.get('/usuario', function(req, res) {
     res.json('get usuario');
 });
-//Agregamos la peticion POST, copiamos el codigo app.get y lo pegamos abajo, cambio get por el post y el mensaje de salida
+// Agregamos la peticion PoST, copiamos el codigo app.get y lo pegamos abajo, cambio get
+// por el post y el mensaje de salida
 app.post('/usuario', function(req, res) {
-    //Con esto podemos recuperar los datos que vienen como valores en las peticiones
+    // Con esto podemos recuperar los datos que vienen como valores en las peticiones
     let body = req.body;
     if (body.nombre === undefined) {
         res.status(400).json({
             ok: false,
             mensaje: 'El nombre es necesario'
         });
-    } else { res.json({ persona: body }); }
+    } else { res.json({ peronsa: body }); }
 });
-//Agregamos la peticion PUT y DELETE, de la misma fomra que el caso anterior
-//Normalmente para poder modificar un registro se le debe indicar a la peticion PUT el id (parámetro) del registro de la siguiente forma
+// Agregamos la peticion PUT y DELETE, de la misma forma que el caso anterior
+// Noralmente para poder modificar un registro se le debe de indicar a la peticion PUT
+// el id (parametro) del registro de la siguiente forma
 app.put('/usuario/:id', function(req, res) {
     //Para recuperar este parametro debemos hacer lo siguiente
     let id = req.params.id;
-    res.json(id);
+    res.json({ id });
 });
-//En el caso del DELETE no se acostumbra a borrar registros solo se cambia un estado de algun registro
-app.delete('/usuario', function(req, res) {
+// En el caso de DELETE no se acostumbra a borrar registros solo se cambia un estado de
+// Algun registro
+app.delete('/', function(req, res) {
     res.json('delete usuario');
 });
-
-//Agregamos una funcion de flecha
-app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto:', process.env.PORT);
-});
-//Es la forma en la que mongoose se va a conectar a mongoDB, mongodb es el protocolo 
-//Por el momento estamos trabajando en el localhost, hay que agregar el puerto en donde esta escuchando mongod (27017)
-//Colocamos el nombre de la base de datos en este caso la llamaremos cafe si no existe en mongoDB la crea, despues
-//Agregamos una funcion de flecha para programar un callback para saber si se logra o no conectar
-mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
-    if (err) throw err; //Si existe un error simplemente se sale del programa
-    console.log('Base de datos ONLINE');
-});
-//Agregamos la funcion de flecha
-app.listen(process.env.PORT, () => {
+// Agregamos una funcion de flecha
+app.listen(3000, () => {
     console.log('Escuchando puerto', process.env.PORT);
 });
